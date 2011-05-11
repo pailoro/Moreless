97de2442c4fbba9a188eea751cf5b7a20aa80c25
Fernando Aureliano <mail@fernandoaureliano.com>
2011-05-11 13:40:36 -0300

    Primeiro Release

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+// ---------- start of jQuery document.ready() ----------
+  
+$(document).ready(function() {   
+
+  //Put your code here
+  
+});
+// ---------- end of jQuery document.ready() ----------
