4edee416cee15343b66d408e1ec01dc7991ec6d5
Fernando Aureliano <mail@fernandoaureliano.com>
2012-06-05 15:18:16 -0300

    js funcitions

--- js/functions.js
+++
@@ -1,8 +0,0 @@
-// ---------- start of jQuery document.ready() ----------
-  
-$(document).ready(function() {   
-
-  //Put your code here
-  
-});
-// ---------- end of jQuery document.ready() ----------
