37173fc5184f40223671507944f2b710b8a9c812
Fernando Aureliano <mail@fernandoaureliano.com>
2012-06-05 15:22:39 -0300

    working on new version

---
+++ js/functions.js
@@ -0,0 +1,23 @@
+// ---------- start of jQuery document.ready() ----------
+  
+$(document).ready(function() {   
+
+	
+	
+	/*
+	jQuery(".menu ul li a").click(function () {
+		
+		if(jQuery('.menu ul li a').hasClass('active')){
+			jQuery('.menu ul li a').removeClass('active');
+		}else{
+			jQuery('.menu ul li a').addClass('active');
+		}
+		
+	});
+*/
+	
+
+  //Put your code here
+  
+});
+// ---------- end of jQuery document.ready() ----------
