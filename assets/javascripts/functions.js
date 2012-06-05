3c79ecd60b425c8c4075c925beeb742855629265
Fernando Aureliano <mail@fernandoaureliano.com>
2012-06-05 15:54:02 -0300

    new version

--- assets/javascripts/functions.js
+++ assets/javascripts/functions.js
@@ -2,7 +2,22 @@
   
 $(document).ready(function() {   
 
+	
+	
+	/*
+	jQuery(".menu ul li a").click(function () {
+		
+		if(jQuery('.menu ul li a').hasClass('active')){
+			jQuery('.menu ul li a').removeClass('active');
+		}else{
+			jQuery('.menu ul li a').addClass('active');
+		}
+		
+	});
+*/
+	
 
+  //Put your code here
   
 });
 // ---------- end of jQuery document.ready() ----------
